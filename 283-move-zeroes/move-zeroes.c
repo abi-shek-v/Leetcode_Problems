dec7af193eb9d224b3567c08c1dfbeefbdd9af93
ABISHEK V <velmurugan11976@gmail.com>
2026-09-21 07:47:30 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (45.83%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.c
@@ -0,0 +1,16 @@
+void moveZeroes(int* nums, int numsSize) {
+     int j = 0;
+    int temp;
+
+    for(int i = 0; i < numsSize; i++) {
+
+        if(nums[i] != 0) {
+
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+
+            j++;
+        }
+    }
+}
