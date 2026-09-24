24ebf2bb47f7b8c336fc391db258cbbb5f0f878b
ABISHEK V <velmurugan11976@gmail.com>
2026-09-24 09:52:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (67.22%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.c
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.c
@@ -6,9 +6,9 @@
  * };
  */
 struct ListNode* middleNode(struct ListNode* head) {
-    struct ListNode* slow=head;
+   struct ListNode* slow=head;
     struct ListNode* fast=head;
-    while(fast!=NULL && fast->next!=NULL){
+    while(fast!=NULL&&fast->next!=NULL){
         slow=slow->next;
         fast=fast->next->next;
     }
